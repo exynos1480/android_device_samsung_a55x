d5473403f79d7f9acd35dba420bf15fe430be806
Yifan Hong <elsk@google.com>
2020-12-10 12:14:58 -0800

    a55x-common: Enforce generic ramdisk allow list
    This change enforces that only the list of files on the generic
    ramdisk can be installed to the ramdisk of this device.
    
    This is required for all devices using generic ramdisk.
    
    Test: m nothing
    Bug: 173742069
    Change-Id: Icff7f30b5aef9fdc59b3060316576f1f50b2a92c

--- common.mk
+++ common.mk
@@ -24,6 +24,9 @@ PRODUCT_CHARACTERISTICS := tablet
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Enforce generic ramdisk allow list
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
 # Soong namespaces
 $(call inherit-product, hardware/samsung_slsi-linaro/config/config.mk)
 
