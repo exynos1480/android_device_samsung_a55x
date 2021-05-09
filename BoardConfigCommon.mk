0d78ab98cc0b3779c8b9487bc2c5825bb487dd40
Nolen Johnson <johnsonnolen@gmail.com>
2021-05-09 16:04:34 -0400

    a55x-common: Simplify AVB flag logic
    * AVB's `--flags 3` argument builds a disabled VBMeta with
      disabled hashtree, so no need to set both flags individually.
    
    Change-Id: Ibe9ee322a5af58f13e76d53e18dee49451fe8838

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -187,10 +187,9 @@ $(call soong_config_set,samsungUsbGadgetVars,gadget_name,13200000.dwc3)
 
 ## Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 BOARD_AVB_VBMETA_SYSTEM := odm product system system_dlkm vendor vendor_dlkm
 BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA4096
