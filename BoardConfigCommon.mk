e887a0062e673973b3e0f1676cf5ef545b4f9838
hachiman770 <unformed8981@gmail.com>
2024-10-21 14:58:46 +0900

    a55x-common: Drop arm architecture support
    Signed-off-by: hachiman770 <unformed8981@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,14 +39,7 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
-
-## Architecture (Secondary)
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-2a
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a76
 
 ## Audio
 $(call soong_config_set,exynos_audio,PREDEFINED_LOW_CAPTURE_DURATION,20)
