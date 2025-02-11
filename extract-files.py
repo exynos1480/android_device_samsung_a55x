4db3299cda0fb7f7211e06403998c9b49ec84f43
Secondarian <robertanitei@tutanota.com>
2025-02-11 17:23:35 +0200

    a55x-common: Initial OpenCL setup

--- extract-files.py
+++ extract-files.py
@@ -54,6 +54,21 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/init/android.hardware.security.keymint-service.samsung.rc': blob_fixup()
         .regex_replace('android\\.hardware\\.security\\.keymint-service\n',
             'android.hardware.security.keymint-service.samsung\n'),
+    'vendor/lib64/hw/vulkan.samsung.so': blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_acquire')
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_getId')
+        .clear_symbol_version('AHardwareBuffer_getNativeHandle')
+        .clear_symbol_version('AHardwareBuffer_release'),
+    'vendor/lib64/libSGPUOpenCL.so': blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_acquire')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_getNativeHandle')
+        .clear_symbol_version('AHardwareBuffer_release'),
+    'vendor/lib64/egl/libGLESv2_samsung.so': blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('ANativeWindow_getFormat'),
     (
         'vendor/lib64/hw/audio.primary.s5e8845.so',
         'vendor/lib64/libaudioproxy2.so',
