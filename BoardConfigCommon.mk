a1ffc9b92a0d8117c39c5c66d9273c63ec890438
Mst8981 <unformed8981@gmail.com>
2025-09-25 14:26:42 +0900

    a55x-common: Add framework_compatibility_matrix.xml and update manifest

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -106,7 +106,9 @@ TARGET_USES_VULKAN := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml
+
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
