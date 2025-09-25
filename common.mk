89be3e3ca11193b2c5a47eceabaf65477032b3c3
devhunter1 <anubhabkundu2@gmail.com>
2025-09-25 14:47:13 +0900

    a55x-common: build light hal from source
    * opensource one working fine right now
    
    Change-Id: Idacb1deacbf1853ed8768cdadf804c710cfada9e

--- common.mk
+++ common.mk
@@ -245,6 +245,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.samsung
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.samsung \
