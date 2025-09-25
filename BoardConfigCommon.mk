ea1334f430eda1454db69e8a76d5313bdd927fd1
Mst8981 <unformed8981@gmail.com>
2025-09-25 09:46:47 +0700

    a55x-common: Bump vendor security patch level to 2025-07-01

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -150,7 +150,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 $(call soong_config_set,cbd,protocol,sipc)
 
 ## Security
-VENDOR_SECURITY_PATCH := 2025-02-01
+VENDOR_SECURITY_PATCH := 2025-07-01
 
 ## SELinux
 BOARD_SEPOLICY_TEE_FLAVOR := teegris
