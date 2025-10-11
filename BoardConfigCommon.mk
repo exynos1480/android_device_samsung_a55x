641a81e590aea6dc4c8c0305c0aa02551cb3062c
Mst8981 <unformed8981@gmail.com>
2025-10-12 07:58:09 +0900

    a55x-common: Update VENDOR_SECURITY_PATCH

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -153,7 +153,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 $(call soong_config_set,cbd,protocol,sipc)
 
 ## Security
-VENDOR_SECURITY_PATCH := 2025-07-01
+VENDOR_SECURITY_PATCH := 2025-09-01
 
 ## SELinux
 BOARD_SEPOLICY_TEE_FLAVOR := teegris
