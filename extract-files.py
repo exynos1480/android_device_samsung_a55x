9d91e9a2e210fd2c0707687781312cd05c5003a9
Flopster101 <nahuelgomez329@gmail.com>
2026-07-08 16:26:33 +0300

    a55x: Fix SoundBooster initialization on AOSP
    * Patch SB_init to initialize as 1 (DISABLED), so that AudioFlinger
      can handle initialization instead of fail with -EINVAL.
    * Patch SB_process to output 8 bytes per stereo frame so that one
      channel doesn't get lost.
    * Patch SB_process to properly bypass DSP for non-speaker outputs
    * Inject missing FF into vendor floating_features.xml
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -138,6 +138,21 @@ blob_fixups: blob_fixups_user_type = {
         .binary_regex_replace(rb'ro\.factory\.factory_binary', b'ro.vendor.factory_binary\x00'),
     'vendor/bin/vaultkeeperd': blob_fixup()
         .binary_regex_replace(rb'ro\.factory\.factory_binary', b'ro.vendor.factory_binary\x00'),
+    'vendor/etc/floating_feature.xml': blob_fixup().regex_replace(
+        r'(?m)^</SecFloatingFeatureSet>$',
+        '    <SEC_FLOATING_FEATURE_AUDIO_CONFIG_SOUNDBOOSTER_LIB_VERSION>2000</SEC_FLOATING_FEATURE_AUDIO_CONFIG_SOUNDBOOSTER_LIB_VERSION>\n'
+        '    <SEC_FLOATING_FEATURE_AUDIO_SUPPORT_SOUNDBOOSTER_ON_DSP>TRUE</SEC_FLOATING_FEATURE_AUDIO_SUPPORT_SOUNDBOOSTER_ON_DSP>\n'
+        '</SecFloatingFeatureSet>',
+    ),
+    'vendor/lib64/soundfx/libsamsungSoundbooster_plus.so': blob_fixup()
+        .sig_replace('bf 01 00 94 48 00 80 52', 'bf 01 00 94 28 00 80 52')
+        .sig_replace('02 f5 7e d3', '02 f1 7d d3')
+        .sig_replace(
+            'a9 52 42 39 a9 06 00 34 f4 03 01 aa a1 05 00 b4 f3 03 02 aa',
+            'f4 03 01 aa f3 03 02 aa a9 52 42 39 69 06 00 34 81 05 00 b4',
+        ),
+    'vendor/lib64/soundfx/libaudiosaplus_sec.so': blob_fixup()
+        .sig_replace('05 00 00 00 02 00 00 00', '05 00 00 00 00 00 00 00'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
